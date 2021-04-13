1a6b4acb94d1a5ecbb7e6304255c7af311bbb2b7
enganddeveloper <46913754+EngandDeveloper@users.noreply.github.com>
2021-04-13 13:00:33 -0400

    Latest

--- src/TheTreasureIsland/Assets/Scripts/Controllers/MainMenuController.cs
+++ src/TheTreasureIsland/Assets/Scripts/Controllers/MainMenuController.cs
@@ -2,16 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class MainMenuController : MonoBehaviour
 {
+    
+    public void changeScene(int index){
+        SceneManager.LoadScene(index);
+    }
+
+    public void openMainMenuScreen(){
+        changeScene(0);
+    }
+
+    public void openSettingsScreen(){
+        changeScene(1);
+    }
+    
+    public void openTutorialScreen(){
+        changeScene(2);
+    }
+
+    public void openHighScoreScreen(){
+        changeScene(3);
+    }
+
+    public void openGamePageScreen(){
+        changeScene(4);
+        PlayerPrefs.SetInt("gameMode", 1);
+    }
+
+    public void openMultipleGamePageScreen(){
+        changeScene(4);
+        PlayerPrefs.SetInt("gameMode", 2);
+    }
     
 
     public void saveSettings(){
 
     }
-    
-    
+
     // Start is called before the first frame update
     void Start()
     {
--- src/TheTreasureIsland/Assets/Scripts/Controllers/ScoreController.cs
+++ src/TheTreasureIsland/Assets/Scripts/Controllers/ScoreController.cs
@@ -4,10 +4,33 @@ using UnityEngine;
 
 public class ScoreController : MonoBehaviour
 {
+
+    float p1Score = 0f; 
+    float p2Score = 0f; 
+
+    public void updateScore(int player, float score){
+        if(player == 1){
+            p1Score += score;
+            PlayerPrefs.SetFloat("p1Score", p1Score);
+        }else if(player == 2){
+            p2Score += score;
+            PlayerPrefs.SetFloat("p2Score", p2Score);
+        }
+    }
+
+    public float getP1Score(){
+        return p1Score;
+    }
+
+    public float getP2Score(){
+        return p2Score;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        p1Score = PlayerPrefs.GetFloat("p1Score");
+        p2Score = PlayerPrefs.GetFloat("p2Score");
     }
 
     // Update is called once per frame
