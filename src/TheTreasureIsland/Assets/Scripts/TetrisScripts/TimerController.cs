63b3a9ed574c27c270ddf00a80f0037e9287607d
Gengyun-Wang <73677146+Gengyun-Wang@users.noreply.github.com>
2021-04-13 23:20:51 -0400

    small modification for variable t

--- src/TheTreasureIsland/Assets/Scripts/TetrisScripts/TimerController.cs
+++ src/TheTreasureIsland/Assets/Scripts/TetrisScripts/TimerController.cs
@@ -9,7 +9,7 @@ public class TimerController : MonoBehaviour
 
     public Text timerText;
 
-
+    public static float t;
     private float startTime;
 
 
@@ -21,7 +21,7 @@ public class TimerController : MonoBehaviour
     }
 
     void Update(){
-        float t = Time.time - startTime;
+        t = Time.time - startTime;
         string minutes = ((int) t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
 
